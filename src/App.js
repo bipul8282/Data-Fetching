13cda85d621972fcc06f3d11e0200eb661f6d305
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2024-02-11 19:15:50 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://randomuser.me/api/?page=1&results=3&seed=abc');
+      const data = await response.json();
+      setUsers(data.results);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.length > 0 && (
+        <div className="main-card">
+          {users.map((user, index) => (
+            <div className="card" key={index}>
+              <div className="image-container">
+                <img src={user.picture.large} alt={`User ${index + 1}`} />
+              </div>
+              <div className="info-card">
+                <h2>{`${user.name.first} ${user.name.last}`}</h2>
+                <p>{user.email}</p>
+                <p>{`${user.location.city}, ${user.location.country}`}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
